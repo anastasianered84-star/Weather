01e512c57a98305707ae08c7a31f9a4babfc4369
DadaKobeleva <kobelevadara@yandex.ru>
2025-12-10 17:02:50 +0500

    DataResponse доделать

---
+++ Models/DataResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Models
+{
+    public class DataResponse
+    {
+        public List<Forecast> forecasts {  get; set; }
+    }
+
+    public class Forecast
+    {
+       public DateTime date {  get; set; }
+        public List<HourOfDay>
+    }
+
+}
